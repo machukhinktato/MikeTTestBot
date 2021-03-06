c847a3aba5318a80484211b17c425a7f30e24a3e
machukhinktato <tarabrinmv@gmail.com>
2021-03-06 10:35:15 +0300

    all works project in  progress

--- main.py
+++ main.py
@@ -16,13 +16,14 @@ Subject: Hi there
 
 This message is sent from Python."""
 
+
 def launch_server():
     context = ssl.create_default_context()
     try:
-        server = smtplib.SMTP(smtp_server,port)
-        server.ehlo() # Can be omitted
-        server.starttls(context=context) # Secure the connection
-        server.ehlo() # Can be omitted
+        server = smtplib.SMTP(smtp_server, port)
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)  # Secure the connection
+        server.ehlo()  # Can be omitted
         server.login(sender_email, sender_email_psw)
         server.sendmail(sender_email, receiver_email, message)
     except Exception as e:
@@ -48,8 +49,15 @@ def get_message():
     return message
 
 
+def send_message(chat_id, text="Hello i'm Mike's bot!"):
+    link = f'sendmessage?chat_id={chat_id}&text={text}'
+    pprint(URL + link)
+    return None
+
+def main():
+    pass
 
 if __name__ == '__main__':
-    pprint(get_message())
+    pprint(send_message(13))
 
-#thats all for today
+# thats all for today
