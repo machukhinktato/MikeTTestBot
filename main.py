9ea4efbc5448ae0dc517b3a1f064890cfa543a38
machukhinktato <tarabrinmv@gmail.com>
2021-03-07 09:57:58 +0300

    in  progress

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import requests
+import json
+import re
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_sslify import SSLify
 from misc.variables import *
 from misc import smtp_server as smtp
 
+app = Flask(__name__)
 
+sslify = SSLify(app)
+
+
+def write_json(data, filename='answer.json'):
+    with open(filename, 'w+') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+"""
 def get_updates():
+    # делаем запросы на сервера телеграма, что не актуально для вебхука
     server_answer = requests.get(URL + 'getupdates').json()
+    # write_json(server_answer)
     return server_answer
 
 
@@ -21,12 +39,43 @@ def get_message():
             'text': message_text,
         }
         return message
+"""
 
 
-def send_message(chat_id, text="Hello i'm Mike's bot!"):
-    requests.get(f"{URL}sendmessage?chat_id={chat_id}&text={text}")
+def send_message(chat_id, text=WELCOME_MESSAGE):
+    requests.post(f"{URL}sendmessage", {'chat_id': chat_id, 'text': text})
 
 
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        user_data = {
+            'subject': '',
+            'title': '',
+            'text': '',
+            'user_id': data["message"]["from"]["id"],
+            'user_name': data["message"]["from"]["username"]
+        }
+        write_json(data)
+        chat_id = data["message"]["chat"]["id"]
+        message = data["message"]["text"]
+        if '/start' in message:
+            send_message(chat_id)
+        if r'написать'.lower() in message:
+            send_message(chat_id, 'введите почту куда отправить (пример: #to example@example.com)')
+
+        if r'#to' in message:
+            send_message(chat_id, 'good')
+        return jsonify(data)
+    return '<h1>Hello im a bot</h1>'
+
+
+def Banana:
+    pass
+
+"""
 def main():
     while True:
         data = get_message()
@@ -34,10 +83,11 @@ def main():
             chat_id = data["chat_id"]
             text = data["text"]
             if '/start' in text:
-                send_message(chat_id, 'hello, how can i help you?')
+                send_message(chat_id)
         else:
             continue
-
+"""
 
 if __name__ == '__main__':
-    main()
+    # main()
+    app.run()
--- misc/variables.py
+++ misc/variables.py
@@ -8,6 +8,12 @@ PORT = 587
 SMTP_SERVER = "smtp.gmail.com"
 global MSG_UPD_ID
 MSG_UPD_ID = 0
+WELCOME_MESSAGE = """
+Уважаемый пользователь, добрый день\n
+Меня зовут Тестбот и я готов Вам помочь в отправлении электронной почты\n
+Для того, чтобы начать оформление письма, используйте команду\n
+/написать\n
+"""
 # RECEIVER_EMAIL = "tarabrinmv@gmail.com"
 # message = """\
 # Subject: Hi there
